cf44af475e7b67d1da89f9a88927e0de94267f09
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-08 00:35:57 +0530

    Add ConfigModule and DatabaseModule to app module
    Integrated ConfigModule as a global module and added DatabaseModule to the imports array in notifications-service.module.ts to enable configuration management and database access.

--- apps/notifications-service/src/notifications-service.module.ts
+++ apps/notifications-service/src/notifications-service.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { NotificationsServiceController } from './notifications-service.controller';
 import { NotificationsServiceService } from './notifications-service.service';
 import { NotificationsModule } from './notifications/notifications.module';
+import { DatabaseModule } from '@app/database';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    NotificationsModule,
+  ],
   controllers: [NotificationsServiceController],
   providers: [NotificationsServiceService],
 })
