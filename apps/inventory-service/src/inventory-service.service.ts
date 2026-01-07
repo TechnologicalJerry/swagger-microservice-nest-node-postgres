de6ae0f0042377d8743a2d6199a1da94ef40bdad
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-08 00:34:54 +0530

    Update greeting message in InventoryServiceService
    Changed the return value of getHello() to 'Inventory Service is running' to provide a more descriptive status message.

--- apps/inventory-service/src/inventory-service.service.ts
+++ apps/inventory-service/src/inventory-service.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class InventoryServiceService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Inventory Service is running';
   }
 }
