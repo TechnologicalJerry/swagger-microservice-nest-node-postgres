ba85535e7f869b359f6d45071e1cc09c068631cb
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-08 00:34:49 +0530

    Add ConfigModule and DatabaseModule to InventoryService
    Integrated ConfigModule as a global module and added DatabaseModule to the imports array in inventory-service.module.ts to enable configuration management and database connectivity.

--- apps/inventory-service/src/inventory-service.module.ts
+++ apps/inventory-service/src/inventory-service.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { InventoryServiceController } from './inventory-service.controller';
 import { InventoryServiceService } from './inventory-service.service';
 import { InventoryModule } from './inventory/inventory.module';
+import { DatabaseModule } from '@app/database';
 
 @Module({
-  imports: [InventoryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    InventoryModule,
+  ],
   controllers: [InventoryServiceController],
   providers: [InventoryServiceService],
 })
