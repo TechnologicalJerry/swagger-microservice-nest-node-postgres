4ac127311da117cdc63251c0addc800666d3e591
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-07 22:23:06 +0530

    Add ConfigModule and DatabaseModule to AuditService
    Integrated ConfigModule as a global module and added DatabaseModule to the imports array in audit-service.module.ts to enable configuration management and database access.

--- apps/audit-service/src/audit-service.module.ts
+++ apps/audit-service/src/audit-service.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuditServiceController } from './audit-service.controller';
 import { AuditServiceService } from './audit-service.service';
 import { AuditModule } from './audit/audit.module';
+import { DatabaseModule } from '@app/database';
 
 @Module({
-  imports: [AuditModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    AuditModule,
+  ],
   controllers: [AuditServiceController],
   providers: [AuditServiceService],
 })
