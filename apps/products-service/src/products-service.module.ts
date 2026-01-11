928476072e2f794d5849e03795898afe00337077
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-11 22:22:21 +0530

    Add ConfigModule and DatabaseModule to ProductsService
    Integrated ConfigModule as a global module and added DatabaseModule to the imports array in ProductsServiceModule. This prepares the service for environment configuration and database access.

--- apps/products-service/src/products-service.module.ts
+++ apps/products-service/src/products-service.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsServiceController } from './products-service.controller';
 import { ProductsServiceService } from './products-service.service';
 import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from '@app/database';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    ProductsModule,
+  ],
   controllers: [ProductsServiceController],
   providers: [ProductsServiceService],
 })
