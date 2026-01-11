673146fe82a7bda3dfb35d9ad8678a13638bd4f8
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-11 22:22:26 +0530

    Update greeting message in ProductsServiceService
    Changed the return value of getHello() to 'Products Service is running' for a more descriptive service status message.

--- apps/products-service/src/products-service.service.ts
+++ apps/products-service/src/products-service.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ProductsServiceService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Products Service is running';
   }
 }
