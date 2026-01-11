5bc99325237a1301b14192a5d3451dcf07d1f28f
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-11 22:21:38 +0530

    Update hello message in PaymentsServiceService
    Changed the return value of getHello() to 'Payments Service is running' for a more descriptive service status message.

--- apps/payments-service/src/payments-service.service.ts
+++ apps/payments-service/src/payments-service.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PaymentsServiceService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Payments Service is running';
   }
 }
