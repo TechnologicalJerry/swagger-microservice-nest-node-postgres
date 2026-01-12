c42d89b42358c54a3527a8e982ed10fe8587b8e7
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-12 21:52:28 +0530

    Update service greeting message
    Changed the return value of getHello() to 'Reports Service is running' for a more descriptive service status message.

--- apps/reports-service/src/reports-service.service.ts
+++ apps/reports-service/src/reports-service.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ReportsServiceService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Reports Service is running';
   }
 }
