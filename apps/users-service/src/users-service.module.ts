b6821deb1744a606c6ff7f0e6d2dbf3454c65e97
TechnologicalJerry <cserajatverma@gmail.com>
2026-01-12 21:52:57 +0530

    Add ConfigModule and DatabaseModule to UsersService
    Integrated ConfigModule with global configuration and added DatabaseModule to the UsersService module imports. This prepares the service for environment-based configuration and database access.

--- apps/users-service/src/users-service.module.ts
+++ apps/users-service/src/users-service.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersServiceController } from './users-service.controller';
 import { UsersServiceService } from './users-service.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from '@app/database';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    UsersModule,
+  ],
   controllers: [UsersServiceController],
   providers: [UsersServiceService],
 })
